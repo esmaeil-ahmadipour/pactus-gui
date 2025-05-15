a4f02aa618568b9776b31bd4e84939f235772159
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2025-05-15 19:38:30 +0330

    add some files

---
+++ .github/scripts/linux/01-install-dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+sudo apt update
+sudo apt install -y libfuse2 build-essential patchelf wget zsync squashfs-tools \
+  libgtk-3-dev libglib2.0-dev libnss3 libxss1 libasound2 libx11-dev
