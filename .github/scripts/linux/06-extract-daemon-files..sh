f64caf71b2e24a15ca20797b560d559a006ff354
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2025-05-15 20:26:56 +0330

    add some files 2

---
+++ .github/scripts/linux/06-extract-daemon-files..sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Set TAG and default value
+TAG_NAME=$(git describe --tags --abbrev=0 2>/dev/null || echo "v1.7.1")
+ARCH="arm64"
+LINUX_ASSETS_URL="https://github.com/pactus-project/pactus/releases/download/${TAG_NAME}/pactus-cli_1.7.1_linux_arm64.tar.gz"
+
+echo "📌 Using TAG_NAME=$TAG_NAME"
+
+# Download files
+echo "⬇️ Downloading daemon file from $LINUX_ASSETS_URL ..."
+curl -L -o file.tar.gz "$LINUX_ASSETS_URL"
+
+# Extract to destination
+DEST="artifacts/linux/${ARCH}/release/bundle/lib/src/core/native_resources/linux/"
+mkdir -p "$DEST"
+echo "📦 Extracting to: $DEST"
+tar -xvzf file.tar.gz --strip-components=1 -C "$DEST"
+
+# Create zip file
+cd "artifacts/linux/${ARCH}/release/bundle"
+FILE_NAME="linux-build-${ARCH}-${TAG_NAME}.zip"
+echo "🗜️ Creating zip: $FILE_NAME"
+zip -r "$FILE_NAME" .
+
+echo "✅ Done. Output zip: $FILE_NAME"
