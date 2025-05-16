4120b01faa764d0a243a6c2edc83a33b4c267017
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2025-05-16 10:47:47 +0330

    add some files 17

--- .github/scripts/linux/07-build-appimage.sh
+++ .github/scripts/linux/07-build-appimage.sh
@@ -4,7 +4,7 @@ set -e
 ./linuxdeploy --appdir AppDir --desktop-file AppDir/pactus_gui.desktop --icon-file AppDir/pactus_gui.png --output appimage
 
 # Dynamic naming with version/tag
-TAG=$(git describe --tags --abbrev=0 2>/dev/null)
+TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "untagged")
 ARCH="x86_64"
 FILE_NAME= "pactus_gui-${TAG}-${ARCH}.AppImage"
 
