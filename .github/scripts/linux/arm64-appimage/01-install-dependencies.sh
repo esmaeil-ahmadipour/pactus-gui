6ef8cf95311d44b71b27521a4151605c749319c6
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2025-05-16 20:46:09 +0330

    add some files 50

--- .github/scripts/linux/arm64-appimage/01-install-dependencies.sh
+++ .github/scripts/linux/arm64-appimage/01-install-dependencies.sh
@@ -2,11 +2,19 @@
 set -e
 
 sudo apt update
+
 sudo apt install -y libfuse2 build-essential patchelf wget zsync squashfs-tools \
-  libgtk-3-dev libglib2.0-dev libnss3 libxss1 libasound2 libx11-dev
-sudo apt install -y tar gzip libssl-dev
-sudo apt install -y unzip
-sudo apt-get install -y qemu-user-static
+  libgtk-3-dev libglib2.0-dev libnss3 libxss1 libx11-dev \
+  tar gzip libssl-dev unzip qemu-user-static cmake ninja-build clang
+
+# Replace problematic libasound2 with real provider
+sudo apt install -y libasound2t64 || sudo apt install -y liboss4-salsa-asound2
+
+# Register emulators (required if building inside docker)
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+# Make linuxdeploy executable
 chmod +x linuxdeploy
+
+# Run with QEMU (assuming linuxdeploy is ARM64 binary)
 qemu-aarch64 ./linuxdeploy --appdir AppDir ...
