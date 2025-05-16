480aafb6883318dd14bb1735f0127eb898e62b17
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2025-05-16 10:39:06 +0330

    add some files 16

--- .github/scripts/linux/04-extract-daemon-files.sh
+++ .github/scripts/linux/04-extract-daemon-files.sh
@@ -2,15 +2,18 @@
 set -e
 
 # Set TAG and default value
-TAG_NAME=$(git describe --tags --abbrev=0 2>/dev/null || echo "v1.7.1")
-ARCH="arm64"
-LINUX_ASSETS_URL="https://github.com/pactus-project/pactus/releases/download/v1.7.1/pactus-cli_1.7.1_linux_amd64.tar.gz"
+TAG_NAME=$(git describe --tags --abbrev=0 2>/dev/null)
+ARCH="x86_64"
+
+# Set default URL if LINUX_ASSETS_URL is empty/unset
+DEFAULT_URL="https://github.com/pactus-project/pactus/releases/download/v1.7.1/pactus-cli_1.7.1_linux_arm64.tar.gz"
+DOWNLOAD_URL="${LINUX_ASSETS_URL:-$DEFAULT_URL}"
 
 echo "📌 Using TAG_NAME=$TAG_NAME"
 
 # Download files
-echo "⬇️ Downloading daemon file from $LINUX_ASSETS_URL ..."
-curl -L -o file.tar.gz "$LINUX_ASSETS_URL"
+echo "⬇️ Downloading daemon file from $DOWNLOAD_URL ..."
+curl -L -o file.tar.gz "$DOWNLOAD_URL"
 
 # Extract to destination
 DEST="build/linux/x64/release/bundle/lib/src/core/native_resources/linux/"
@@ -18,4 +21,4 @@ mkdir -p "$DEST"
 echo "📦 Extracting to: $DEST"
 tar -xvzf file.tar.gz --strip-components=1 -C "$DEST"
 
- echo "✅ Done. Output AppImage: $FILE_NAME"
+ echo "✅ Done. Output AppImage"
