78ce1b487e195701df69a8461493400bb4915ebc
Esmaeil Ahmadipour <ahmadipour1402@gmail.com>
2025-06-04 15:41:03 +0330

    fix: fix flutter version (12)

--- __github/scripts/linux/arm64-appimage/08-test.sh
+++ __github/scripts/linux/arm64-appimage/08-test.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Test ARM64 AppImage in emulator if needed
-qemu-aarch64 ./pactus_gui*-arm64.AppImage --appimage-extract
+qemu-aarch64 ./*arm64*.AppImage --appimage-extract
 chmod +x squashfs-root/AppRun
 squashfs-root/AppRun --version
